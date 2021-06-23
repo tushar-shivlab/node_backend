1b4200c16c6ad62c52c97a46ed4e68736da8fdf0
tushar-shivlab <80961599+tushar-shivlab@users.noreply.github.com>
2021-06-23 11:41:44 +0530

    Add files via upload

---
+++ src/enum/InternalLinks.js
@@ -0,0 +1,11 @@
+export default {
+    BASE_API_URL: "/api/v1/",
+    USER: {
+      BASE_URL: "/api/v1/user",
+      GROUP_JOIN:"/group/join"
+    },
+    GROUP:{
+      BASE_URL: "/api/v1/group",
+      GET:'/:id'
+    }
+  };
