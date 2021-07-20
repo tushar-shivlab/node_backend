ca5398d0a5475c91494d8ba730e519465862f590
tushar-shivlab <80961599+tushar-shivlab@users.noreply.github.com>
2021-07-20 12:53:06 +0530

    Update expressInit.js

--- src/lib/expressInit.js
+++ src/lib/expressInit.js
@@ -19,7 +19,7 @@ const expressInit = (server) => {
         /** CORS */
         server.use(
             cors({
-                origin: 'http://localhost:3000',
+                origin: ['http://localhost:3000','https://node-special-engine.herokuapp.com/],
                 methods: ['GET', 'POST'],
             }),
         );
